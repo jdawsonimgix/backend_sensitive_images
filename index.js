09cbe9a9d91681e116b1283c4f760d8091cb8a0c
jdawsonimgix <jdawson@imgix.com>
2022-12-02 15:58:21 -0800

    testing uploadToImgix command

--- index.js
+++ index.js
@@ -2,14 +2,58 @@
 const express = require("express");
 const axios = require("axios");
 const home = require("./routes/home");
+const multer = require("multer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 require("dotenv").config();
 
 // Middlewares
 const app = express();
+const memoStorage = multer.memoryStorage();
+const upload = multer({ memoStorage });
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 // Routes
 app.use("/home", home);
+
+//Uploading a file to imgix.
+app.post("/uploadToImgix", upload.single("pic"), async (req, res) => {
+  const file = req.file;
+  console.log("/uploadToImgix in server.js");
+  console.log(file);
+
+  var config = {
+    method: "post",
+    url:
+      `https://api.imgix.com/api/v1/sources/upload/62e31fcb03d7afea23063596/` +
+      file.originalname,
+    headers: {
+      Authorization: "Bearer " + process.env.IMGIX_API,
+      "Content-Type": file.mimetype,
+    },
+    data: req.file.buffer,
+  };
+
+  let final = await axios(config)
+    .then(function (response) {
+      console.log("successful call from /uploadToImgix in server.js");
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return error;
+    });
+
+  let trueFinal = {
+    allData: final,
+    theBufferReturned: req.file.buffer,
+  };
+  return res.status(200).send(trueFinal);
+});
 
 //Get sensitive data status:
 app.get("/testimgix", async (req, res) => {
