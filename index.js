6897c51132b66313ff4a267071e023025d848290
jdawsonimgix <jdawson@imgix.com>
2022-12-02 13:13:40 -0800

    created testimgix command

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Import packages
 const express = require("express");
+const axios = require("axios");
 const home = require("./routes/home");
+require("dotenv").config();
 
 // Middlewares
 const app = express();
@@ -8,6 +10,31 @@ app.use(express.json());
 
 // Routes
 app.use("/home", home);
+
+//Get sensitive data status:
+app.get("/testimgix", async (req, res) => {
+  console.log("ran /testimgix");
+  var config = {
+    method: "get",
+    url: "https://api.imgix.com/api/v1/assets/62e31fcb03d7afea23063596/circuitttt.jpeg",
+    headers: {
+      Authorization: "Bearer " + process.env.IMGIX_API,
+      "Content-Type": "image/jpeg",
+      "Accept-Encoding": "application/json",
+    },
+  };
+
+  let result = await axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return error;
+    });
+  return res.status(200).send(result);
+});
 
 // connection
 const port = process.env.PORT || 9001;
